9e105731b9fda0ee75eae65efe4374e46e0b76f4
Eugene Safronov <evgeniy.safronov@outlook.com>
2017-06-12 11:14:49 +0300

    setup vue-resource

---
+++ src/components/Artist.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="artist">
+    <h1>{{ title }}</h1>
+    <form>
+      <label for="firstName">First name:</label>
+      <input id="firstName" v-model="artist.first_name"><br/>
+      <label for="firstName">Last name:</label>
+      <input id="firstName" v-model="artist.last_name"><br/>
+      <label for="firstName">Email name:</label>
+      <input id="firstName" v-model="artist.email"><br/>
+      <button v-on:click="sibmitData">Save</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'artist',
+  data () {
+    return {
+      title: 'Welcome to Your Vue.js App',
+      artist: {
+        first_name: '',
+        last_name: '',
+        email: ''
+      }
+    }
+  },
+  methods: {
+    sibmitData: function () {
+      this.$http.post('http://localhost:3000/artists', JSON.stringify(this.artist))
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import VueResource from 'vue-resource'
+import BootstrapVue from 'bootstrap-vue'
+
 import Hello from '@/components/Hello'
+import Artist from '@/components/ARTIST'
 
 Vue.use(Router)
+Vue.use(VueResource)
+Vue.use(BootstrapVue)
 
 export default new Router({
   routes: [
@@ -10,6 +16,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/artists/new',
+      name: 'Artist',
+      component: Artist
     }
   ]
 })
